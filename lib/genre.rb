d4d61646e428199ec1c81db2b9714f791158e10f
Melissa Williams <melissa@brandmeetscopy.com>
2019-06-28 22:39:15 +0000

    created Genre class

--- lib/genre.rb
+++ lib/genre.rb
@@ -2,11 +2,17 @@ class Genre
   
   @@all = []
   
-  attr_accessor :authors, :titles 
+  attr_accessor :name, :books, :authors, :url
   
   def self.all 
     @@all 
   end 
+  
+  def initialize(name: name, url: url)
+    @name = name
+    @url = url
+    @@all << self
+  end
   
   
   
