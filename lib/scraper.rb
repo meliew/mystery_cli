a76f597875267153d292d19dc35d2466ac03a97f
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-02 00:20:11 +0000

    updated scraper methods

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,71 +1,60 @@
-class Scraper 
-  
+class Scraper
 
-  def get_genre_list 
-    site = "https://www.goodreads.com"  
-    page = Nokogiri::HTML(open(site+"/genres/mystery"))   
+  def genre_scraper
+    site = "https://www.goodreads.com"
+    page = Nokogiri::HTML(open(site+"/genres/mystery"))
     results = page.css(".left .gr-hyperlink")
 
     genres = {}
 
     results.each.with_index do |r, i|
-      if r.text != "Fiction"
+      if r.text != "Fiction" && r.text != "Murder Mystery"
         genres[i] = {:genre => r.text, :url => site+r.attribute("href").value}
+      end
     end
-  end
 
     genres.each do |k, g|
       puts "#{k}: #{g[:genre]}"
+    end
+
+
+    puts "Choose the number genre you would like:"
+    input = gets.chomp.to_i
+
+    g = genres[input]
+    if !g
+      puts "That item does not exist "
+      return
+    end
+    @genre = g
   end
 
-  puts "Choose the number genre you would like:"
-  input = gets.chomp.to_i
-
-  g = genres[input]
-  if !g
-    puts "That item does not exist "
-    return
-  end
-
-  @genre = g
-  
-end
-  
-  def about_genre_scraper 
+  def about_genre_scraper
     site = @genre[:url]
-    page = Nokogiri::HTML(open(site))   
-    results = page.css("div.mediumText")[0]
-    puts results.text
+    page = Nokogiri::HTML(open(site))
+    results = page.css("div.mediumText.reviewText span")
+    bio = results[1] || results[0]
+    #  results = page.css("div.mediumText span").children[0]
+
+    puts bio.text
+
   end
 
+  def new_release_scraper
+    site = @genre[:url]
+    page = Nokogiri::HTML(open(site), nil, Encoding::UTF_8.to_s)
+    results = page.css(".leftContainer .bigBoxBody").first.css(".bookImage")
+    results.each do |book|
+
+      puts book.attribute("alt").text
+    end
+
+  end
 
 end
 
 
 
-# site = "https://www.goodreads.com/genres/mystery"
-    
-#     page = Nokogiri::HTML(open(site))
-    
-#     results = page.css(".left").css(".gr-hyperlink")
 
-#     results.each.with_index do |r, i|
-    
-#     if r.text != "Fiction"
-
-#     puts "#{i}. #{r.text}"
-
-
-# results_array.each do |genre|
-# puts "#{genre[0]}"
-# end 
-
-
-
-# root = 
-# url = 
-# showpage = Nokogiri::HTML(open(""))
-
-# title
 
 
