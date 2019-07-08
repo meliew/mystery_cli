2b0f9292556d212354b46f736531a46419bc578b
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-08 20:10:53 +0000

    updated text in CLI

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,6 +4,7 @@ class CLI
   
   def initialize
     intro
+    sleep 1
     Scraper.genre_scraper 
     list_genres
     get_user_input
@@ -13,17 +14,17 @@ class CLI
   end
   
   def intro
-    puts "\nWelcome to Choose Your Next Mystery"
+    puts "\nWelcome to Choose Your Next Mystery! Did you know there are many subgenres related to the mystery genre? This tool will help you select a new type of mystery and can show you new releases as well. Let's get started!"
   end
   
   def list_genres
-    puts "Here are the genres related to mystery"
+    puts "Here are the subgenres related to the mystery genre"
     Genre.all.each{ |i, g| puts "#{i}. #{g.name}" }
   end
   
   def get_user_input
     genre_input = nil
-    puts "\nChoose your next mystery by selecting the number of the genre you want:"
+    puts "\nLearn more about a genre by selecting the number next to the one want to hear about. You can also type exit if you'd like to quit the program"
     
     genre_input = gets.chomp.downcase
     @selected_genre = Genre.all[genre_input.to_i]
@@ -41,7 +42,7 @@ class CLI
   end
 
   def more_info
-    puts "Would you like to see a list of new releases for your genre?(type y/n)"
+    puts "Would you like to see a list of new releases for your genre?(type y for yes/n for no)"
     answer = gets.chomp.downcase 
     if answer == "y"
       show_books
